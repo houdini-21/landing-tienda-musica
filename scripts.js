c12877e9b1871b6f5b111493f2ca64653f26126d
Fernando Jose Marinero Cruz <marinerocruzfer@gmail.com>
2020-06-23 13:07:01 -0600

    botones activos segun seccion activa

---
+++ scripts.js
@@ -0,0 +1,27 @@
+function changeclass1() {
+  document.getElementById("navbutton1").classList.add("active");
+  document.getElementById("navbutton2").classList.remove("active");
+  document.getElementById("navbutton3").classList.remove("active");
+  document.getElementById("navbutton4").classList.remove("active");
+}
+
+function changeclass2() {
+    document.getElementById("navbutton2").classList.add("active");
+    document.getElementById("navbutton1").classList.remove("active");
+    document.getElementById("navbutton3").classList.remove("active");
+    document.getElementById("navbutton4").classList.remove("active");
+  }
+
+  function changeclass3() {
+    document.getElementById("navbutton3").classList.add("active");
+    document.getElementById("navbutton1").classList.remove("active");
+    document.getElementById("navbutton2").classList.remove("active");
+    document.getElementById("navbutton4").classList.remove("active");
+  }
+  function changeclass4() {
+    document.getElementById("navbutton4").classList.add("active");
+    document.getElementById("navbutton1").classList.remove("active");
+    document.getElementById("navbutton2").classList.remove("active");
+    document.getElementById("navbutton3").classList.remove("active");
+  }
+  
