eaf1f8a2c444f2537828c7398cd63d027c534ffc
Fernando Jose Marinero Cruz <marinerocruzfer@gmail.com>
2020-06-24 09:25:39 -0600

    casi listo aun falta validaciones en formulario y marcar en la barra de navegacion donde estas

--- music.js
+++ music.js
@@ -1,5 +1,13 @@
 let play1 = new Audio("./music/marioneta.mp3");
-let play2 = new Audio("./music/reyyas.mp3");
+let play2 = new Audio("./music/rey&as.mp3");
+let play3 = new Audio("./music/fosterthepeople.mp3");
+let play4 = new Audio("./music/rezz.mp3");
+let play5 = new Audio("./music/daftpunk.mp3");
+let play6 = new Audio("./music/ninocohete.mp3");
+let play7 = new Audio("./music/porterrobinson.mp3");
+let play8 = new Audio("./music/madeon.mp3");
+let play9 = new Audio("./music/echosmith.mp3");
+let play10 = new Audio("./music/pixies.mp3");
 
 document.getElementById("play1").addEventListener("click", function () {
   const icon = document.getElementById("playbutton1");
@@ -29,46 +37,92 @@ document.getElementById("play3").addEventListener("click", function () {
   const icon = document.getElementById("playbutton3");
   const pause = icon.classList.toggle("fa-pause");
   const play = icon.classList.toggle("fa-play");
+  if (pause === true) {
+    play3.play();
+  } else {
+    play3.pause();
+  }
 });
 
 document.getElementById("play4").addEventListener("click", function () {
   const icon = document.getElementById("playbutton4");
   const pause = icon.classList.toggle("fa-pause");
   const play = icon.classList.toggle("fa-play");
+
+  if (pause === true) {
+    play4.play();
+  } else {
+    play4.pause();
+  }
 });
 
 document.getElementById("play5").addEventListener("click", function () {
   const icon = document.getElementById("playbutton5");
   const pause = icon.classList.toggle("fa-pause");
   const play = icon.classList.toggle("fa-play");
+
+  if (pause === true) {
+    play5.play();
+  } else {
+    play5.pause();
+  }
 });
 
 document.getElementById("play6").addEventListener("click", function () {
   const icon = document.getElementById("playbutton6");
   const pause = icon.classList.toggle("fa-pause");
   const play = icon.classList.toggle("fa-play");
+  if (pause === true) {
+    play6.play();
+  } else {
+    play6.pause();
+  }
 });
 
 document.getElementById("play7").addEventListener("click", function () {
   const icon = document.getElementById("playbutton7");
   const pause = icon.classList.toggle("fa-pause");
   const play = icon.classList.toggle("fa-play");
+
+  if (pause === true) {
+    play7.play();
+  } else {
+    play7.pause();
+  }
 });
 
 document.getElementById("play8").addEventListener("click", function () {
   const icon = document.getElementById("playbutton8");
   const pause = icon.classList.toggle("fa-pause");
   const play = icon.classList.toggle("fa-play");
+
+  if (pause === true) {
+    play8.play();
+  } else {
+    play8.pause();
+  }
 });
 
 document.getElementById("play9").addEventListener("click", function () {
   const icon = document.getElementById("playbutton9");
   const pause = icon.classList.toggle("fa-pause");
   const play = icon.classList.toggle("fa-play");
+
+  if (pause === true) {
+    play9.play();
+  } else {
+    play9.pause();
+  }
 });
 
 document.getElementById("play10").addEventListener("click", function () {
   const icon = document.getElementById("playbutton10");
   const pause = icon.classList.toggle("fa-pause");
   const play = icon.classList.toggle("fa-play");
+
+  if (pause === true) {
+    play10.play();
+  } else {
+    play10.pause();
+  }
 });
--- scripts.js
+++ scripts.js
@@ -1,30 +1,15 @@
-function changeclass1() {
-  document.getElementById("navbutton1").classList.add("active");
-  document.getElementById("navbutton2").classList.remove("active");
-  document.getElementById("navbutton3").classList.remove("active");
-  document.getElementById("navbutton4").classList.remove("active");
+const links = document.querySelectorAll(".navbar ul a");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
 }
 
-function changeclass2() {
-  document.getElementById("navbutton2").classList.add("active");
-  document.getElementById("navbutton1").classList.remove("active");
-  document.getElementById("navbutton3").classList.remove("active");
-  document.getElementById("navbutton4").classList.remove("active");
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+
+  document.querySelector(href).scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
 }
-
-function changeclass3() {
-  document.getElementById("navbutton3").classList.add("active");
-  document.getElementById("navbutton1").classList.remove("active");
-  document.getElementById("navbutton2").classList.remove("active");
-  document.getElementById("navbutton4").classList.remove("active");
-}
-function changeclass4() {
-  document.getElementById("navbutton4").classList.add("active");
-  document.getElementById("navbutton1").classList.remove("active");
-  document.getElementById("navbutton2").classList.remove("active");
-  document.getElementById("navbutton3").classList.remove("active");
-}
-
-
-
-
