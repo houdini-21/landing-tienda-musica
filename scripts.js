2aedb8a583ba466a99d00f3649e7b6a37a58a9ff
Fernando Jose Marinero Cruz <marinerocruzfer@gmail.com>
2020-06-25 08:06:22 -0600

    ya funciona, aun asi no es suficiente

--- scripts.js
+++ scripts.js
@@ -1,10 +1,9 @@
 const links = document.querySelectorAll(".navbar ul a");
 let itemMenu = document.querySelectorAll(".container");
-let inicio = document.getElementById('inicionav')
-let categorias = document.getElementById('categoriasnav')
-let vendidos = document.getElementById('masvendidosnav')
-let contactanos = document.getElementById('contactanosnav')
-
+let inicio = document.getElementById("inicionav");
+let categorias = document.getElementById("categoriasnav");
+let vendidos = document.getElementById("masvendidosnav");
+let contactanos = document.getElementById("contactanosnav");
 
 for (const link of links) {
   link.addEventListener("click", clickHandler);
@@ -20,45 +19,40 @@ function clickHandler(e) {
   });
 }
 
-window.addEventListener("scroll", ()=> {
-  if (scrollY >= 0 && scrollY <= 755){
-    inicio.classList.add('active')
-    vendidos.classList.remove('active')
-    contactanos.classList.remove('active')
-    categorias.classList.remove('active')
+window.addEventListener("scroll", () => {
+  if (scrollY >= 0 && scrollY <= 755) {
+    inicio.classList.add("active");
+    vendidos.classList.remove("active");
+    contactanos.classList.remove("active");
+    categorias.classList.remove("active");
+  } else if (scrollY >= 758 && scrollY <= 1420) {
+    categorias.classList.add("active");
+    vendidos.classList.remove("active");
+    contactanos.classList.remove("active");
+    inicio.classList.remove("active");
+  } else if (scrollY >= 1423 && scrollY <= 2165) {
+    vendidos.classList.add("active");
+    categorias.classList.remove("active");
+    contactanos.classList.remove("active");
+    inicio.classList.remove("active");
+  } else if (scrollY >= 2166 && scrollY <= 2422) {
+    contactanos.classList.add("active");
+    vendidos.classList.remove("active");
+    categorias.classList.remove("active");
+    inicio.classList.remove("active");
   }
-
-  else if(scrollY >= 758 && scrollY <= 1420) {
-  categorias.classList.add('active')
-  vendidos.classList.remove('active')
-  contactanos.classList.remove('active')
-  inicio.classList.remove('active')
-  }
-  
-  else if (scrollY >= 1423 && scrollY <= 2165){
-    vendidos.classList.add('active')
-    categorias.classList.remove('active')
-    contactanos.classList.remove('active')
-    inicio.classList.remove('active')
-
-  }
-  else if ( scrollY >= 2166 && scrollY <= 2422 ){
-    contactanos.classList.add('active')
-    vendidos.classList.remove('active')
-    categorias.classList.remove('active')
-    inicio.classList.remove('active')
-
-  }
-})
-
+});
 
 document.getElementById("like").addEventListener("click", function () {
   const icon = document.getElementById("heart");
-  const pause = icon.classList.toggle("fal");
-  const play = icon.classList.toggle("fas");
+  const dislike = icon.classList.toggle("fal");
+  const like = icon.classList.toggle("fas");
 });
 
-
+function like() {
+  const dislike = icon.classList.toggle("fal");
+  const like = icon.classList.toggle("fas");
+}
 
 /* 
 inicio offsetTop = 0
